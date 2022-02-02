e38512a2d22bd19d46c7fe8ece0358ef84d686ed
Gwendal <gwetre@gmail.com>
2022-02-02 18:27:25 +0100

    update

--- scripts_bash/cow_university.sh
+++ scripts_bash/cow_university.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-if [ $1 -gt 1 ]
-then
-for i in $(eval echo {2..$(expr 1 + $(expr $1 / 2 ))})
-do
-if [ $(expr $1 % $i) -ne 0 ]
-then
-cowsay $i
+j=1
+cowsay 2
 sleep 1
+while [ $j -le $1 ]
+do
+if [ $j -gt 1 ]
+then
+estPremier=1
+for i in $(eval echo {2..$(expr 1 + $(expr $j / 2 ))})
+do
+
+if [ $(expr $j % $i) -eq 0 ]
+then
+estPremier=0
 fi
 done
 fi
+if [[ $estPremier -eq 1 ]]
+then
+cowsay $j
+sleep 1
+fi
+j=$(expr $j + 1)
+done
 cowsay -T \U meuh
